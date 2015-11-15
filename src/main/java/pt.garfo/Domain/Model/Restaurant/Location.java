afbb9ddf7505b1e588b2e48a804ea95ece15c4c1
marcelopereirascmspain <marcelo.pereira@scmspain.com>
2015-11-15 23:56:48 +0100

    Make location with doubles instead of strings and validate the values.

--- src/main/java/pt.garfo/Domain/Model/Restaurant/Location.java
+++ src/main/java/pt.garfo/Domain/Model/Restaurant/Location.java
@@ -1,19 +1,37 @@
 package pt.garfo;
 
 public class Location {
-  private String latitude;
-  private String longitude;
+  private final Double MIN_LATITUDE = -90.0;
+  private final Double MAX_LATITUDE = 90.0;
+  private final Double MIN_LONGITUDE = -180.0;
+  private final Double MAX_LONGITUDE = 180.0;
+  
+  private Double latitude;
+  private Double longitude;
 
-  public Location(String latitude, String longitude) {
+  public Location(Double latitude, Double longitude) {
     this.latitude = latitude;
     this.longitude = longitude;
+    this.validate();
   }
 
-  public String latitude() {
+  private void validate() {
+    if (this.latitude < MIN_LATITUDE || this.latitude > MAX_LATITUDE) {
+      throw new IllegalArgumentException(
+        "Latitude must be within the -90deg and +90deg range.");
+    }
+
+    if (this.longitude < MIN_LONGITUDE || this.longitude > MAX_LONGITUDE) {
+      throw new IllegalArgumentException(
+        "Longitude must be within the -180deg and +180deg range.");
+    }
+  }
+
+  public Double latitude() {
     return this.latitude;
   }
 
-  public String longitude() {
+  public Double longitude() {
     return this.longitude;
   }
 }
